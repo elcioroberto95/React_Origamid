8c2de6bb2d0848df129969268a88e699d7b9a61c
Elcio <elcioroberto1995@gmail.com>
2020-11-06 18:55:21 -0300

    mudancas

--- src/App.js
+++ src/App.js
@@ -1,101 +1,27 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Produtos from './Components/Produtos';
+import Contato from './Components/Contato';
+import Produto from './Components/Produto';
+import Footer from './Components/Footer';
 const App = () => {
-  const formFields = [
-    {
-      id: 'nome',
-      label: 'nome',
-      type: 'text',
-    },
-    {
-      id: 'email',
-      label: 'email',
-      type: 'email',
-    },
-    {
-      id: 'senha',
-      label: 'senha',
-      type: 'password',
-    },
-    {
-      id: 'cep',
-      label: 'cep',
-      type: 'text',
-    },
-    {
-      id: 'rua',
-      label: 'rua',
-      type: 'text',
-    },
-    {
-      id: 'numero',
-      label: 'numero',
-      type: 'text',
-    },
-    {
-      id: 'bairro',
-      label: 'bairro',
-      type: 'text',
-    },
-    {
-      id: 'cidade',
-      label: 'cidade',
-      type: 'text',
-    },
-    {
-      id: 'estado',
-      label: 'estado',
-      type: 'text',
-    },
-  ];
-  const formFieldsReduce = formFields.reduce((acc, field) => {
-    console.log(acc);
-    return {
-      ...acc,
-      [field.id]: '',
-    };
-  }, {});
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-    senha: '',
-    cep: '',
-    rua: '',
-    numero: '',
-    bairro: '',
-    cidade: '',
-    estado: '',
-  });
-  const [response, setResponse] = React.useState(null);
-  function handleChange(event) {
-    const { id, value } = event.target;
-    setForm({ ...form, [id]: value });
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    fetch(`https://ranekapi.origamid.dev/json/api/usuario`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(form),
-    }).then((response) => {
-      setResponse(response);
-    });
-  }
   return (
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({ id, label, type }) => (
-        <div key={id}>
-          <label htmlFor={label}>{label}</label>
-          <input type={type} id={id} value={form[id]} onChange={handleChange} />
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
         </div>
-      ))}
 
-      {response && response.ok && <p>Vai se fuder</p>}
-      {response && response.ok == false && <p>Vai toma no seu CU</p>}
-      <button>Enviar</button>
-    </form>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 };
 
